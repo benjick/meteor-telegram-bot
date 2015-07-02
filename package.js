a02d4dac250dad8a570f298e96cd41fb19742606
Max Malm <benjick@dumfan.net>
2015-07-02 11:05:02 +0200

    pre-publish fixes

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
   name: 'benjick:telegram-bot',
   version: '0.1.0',
   summary: 'TelegramBot API wrapper',
-  git: 'https://github.com/benjick/meteor-telegram-api',
+  git: 'https://github.com/benjick/meteor-telegram-bot',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use(['meteorhacks:picker', 'http'], 'server');
+  api.use(['meteorhacks:picker@1.0.0', 'http'], 'server');
   Npm.depends({"body-parser": "1.13.1"})
   api.addFiles(['routes.js', 'telegram-bot.js'], 'server');
   api.export('TelegramBot', 'server');
