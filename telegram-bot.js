087e5af63226c974c7918c18705b09ad2209b071
dcsan <dc@pikkle.com>
2015-08-16 04:13:29 -0700

    inconsistency reapplied

--- telegram-bot.js
+++ telegram-bot.js
@@ -6,7 +6,7 @@ TelegramBot.init = false;
 TelegramBot.getUpdatesOffset = 0;
 TelegramBot.interval = false;
 
-TelegramBot.parseCommandString = function(msg){
+TelegramBot.parseCommandString = function(msg) {
 	// splits string into an array
 	// and removes the @botname from the command
 	// then returns the array
@@ -15,32 +15,32 @@ TelegramBot.parseCommandString = function(msg){
 	return msg;
 }
 
-TelegramBot.poll = function(){
+TelegramBot.poll = function() {
 	var result = TelegramBot.method("getUpdates", {
 		offset: TelegramBot.getUpdatesOffset + 1
 	});
 	TelegramBot.parsePollResult(result.result);
 }
 
-TelegramBot.start = function(){
+TelegramBot.start = function() {
 	TelegramBot.poll();
-	TelegramBot.interval = Meteor.setInterval(function(){
+	TelegramBot.interval = Meteor.setInterval(function() {
 		TelegramBot.poll();
 	}, 1000);
 }
 
-TelegramBot.stop = function(){
+TelegramBot.stop = function() {
 	Meteor.clearInterval(TelegramBot.interval);
 }
 
-TelegramBot.parsePollResult = function(data){
-	data.map(function(item){
+TelegramBot.parsePollResult = function(data) {
+	data.map(function(item) {
 		TelegramBot.getUpdatesOffset = item.update_id;
 		var chatId = item.message.chat.id;
 		var from = item.message.from.username;
 		if(msg = item.message.text) {
 			msg = TelegramBot.parseCommandString(msg)
-			var obj = _.find(TelegramBot.triggers, function(obj){
+			var obj = _.find(TelegramBot.triggers, function(obj) {
 				return obj.command == msg[0]
 			})
 			if(obj) {
@@ -54,12 +54,12 @@ TelegramBot.parsePollResult = function(data){
 	});
 }
 
-TelegramBot.requestUrl = function(method){
+TelegramBot.requestUrl = function(method) {
 	var token = TelegramBot.token || process.env.TELEGRAM_TOKEN;
 	return TelegramBot.apiBase + token + '/' + method
 }
 
-TelegramBot.addListener = function(command, callback){
+TelegramBot.addListener = function(command, callback) {
 	if(typeof(command) === 'string' && typeof(callback) === 'function') {
 		TelegramBot.triggers.push({
 			command: command,
@@ -72,7 +72,7 @@ TelegramBot.addListener = function(command, callback){
 	}
 }
 
-TelegramBot.method = function(method, object){
+TelegramBot.method = function(method, object) {
 	var object = object || {};
 
 	try {
@@ -88,7 +88,7 @@ TelegramBot.method = function(method, object){
 	}
 }
 
-TelegramBot.send = function(msg, chatId){
+TelegramBot.send = function(msg, chatId) {
 	if(!msg) {
 		return false;
 	}
