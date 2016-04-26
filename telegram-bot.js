8442ccfb77b96988eaabdffb08272ea71ab79968
Elie Steinbock <eliesteinbock@gmail.com>
2016-04-26 14:42:24 +0300

    cleaned up code and made a small fix in parsePollResult

--- telegram-bot.js
+++ telegram-bot.js
@@ -1,14 +1,18 @@
 TelegramBot = {};
 TelegramBot.triggers = {};
 TelegramBot.conversations = {};
-TelegramBot.catchAllText = { enabled: false, callback: function(username, message) {console.log('Default catchAll Method. Received: ' + message);} };
+TelegramBot.conversations = {};
+TelegramBot.catchAllText = {
+	enabled: false,
+	callback: (username, message) => console.log('Default catchAll Method. Received: ' + message)
+};
 TelegramBot.apiBase = "https://api.telegram.org/bot";
 TelegramBot.token = false;
 TelegramBot.init = false;
 TelegramBot.getUpdatesOffset = 0;
 TelegramBot.interval = false;
 
-TelegramBot.parseCommandString = function(msg) {
+TelegramBot.parseCommandString = msg => {
 	// splits string into an array
 	// and removes the @botname from the command
 	// then returns the array
@@ -17,59 +21,65 @@ TelegramBot.parseCommandString = function(msg) {
 	return msg;
 }
 
-TelegramBot.poll = function() {
+TelegramBot.poll = () => {
 	const result = TelegramBot.method('getUpdates', {
 		offset: TelegramBot.getUpdatesOffset + 1,
 	});
 	
 	// Additional check for duplicate poll data
 	// Also skips the call to parsePollResults if there were no results
-	if(result && result.result.length > 0 && TelegramBot.getUpdatesOffset !==  _.last(result.result).update_id)
+	if (result && result.result.length > 0 && TelegramBot.getUpdatesOffset !==  _.last(result.result).update_id)
 		TelegramBot.parsePollResult(result.result);
 }
 
-TelegramBot.start = function() {
+TelegramBot.start = () => {
 	TelegramBot.poll();
-	TelegramBot.interval = Meteor.setInterval(() => {
-		TelegramBot.poll();
-	}, 1000);
+	TelegramBot.interval = Meteor.setInterval(TelegramBot.poll, 1000);
 }
 
-TelegramBot.stop = function() {
+TelegramBot.stop = () => {
 	Meteor.clearInterval(TelegramBot.interval);
 }
 
-TelegramBot.parsePollResult = function(data) {
+TelegramBot.parsePollResult = data => {
 	data.map(item => {
 		TelegramBot.getUpdatesOffset = item.update_id;
 
 		const message = item.message;
-		keys = Object.keys(message);
-		if (keys[keys.length-1] == "entities")
-			keys.pop();
-		const type = keys.pop();
-		const from = item.message.from.username;
-		const chatId = message.chat.id;
-		var is_conversation = false;
-		
-		if(typeof(TelegramBot.conversations[chatId]) !== 'undefined') {
-			const obj = _.find(TelegramBot.conversations[chatId], obj => obj.username == from);
-			if(obj) {
-				is_conversation = true;
-				obj.callback(from, message.text, chatId);
+
+		if (message) {
+			const keys = Object.keys(message);
+			
+			if (keys[keys.length - 1] === "entities")
+				keys.pop();
+			
+			const type = keys.pop();
+			const fromUsername = item.message.from.username;
+			const chatId = message.chat.id;
+			let isConversation = false;
+			
+			if (typeof(TelegramBot.conversations[chatId]) !== 'undefined') {
+				const obj = _.find(TelegramBot.conversations[chatId], obj => obj.username === fromUsername);
+				
+				if (obj) {
+					isConversation = true;
+					obj.callback(fromUsername, message.text, chatId);
+				}
 			}
-		}
-		if(!is_conversation) {
-			if(type === 'text' && typeof(TelegramBot.triggers.text) !== 'undefined') {
-				const msg = TelegramBot.parseCommandString(item.message.text);
-				const obj = _.find(TelegramBot.triggers.text, obj => obj.command == msg[0]);
-				if(obj) {
-					TelegramBot.send(obj.callback(msg, from, message), chatId);
-				} else if(TelegramBot.catchAllText.enabled) TelegramBot.catchAllText.callback(from, message);
-			} else {
-				if(typeof(TelegramBot.triggers[type]) !== 'undefined') {
+			
+			if (!isConversation) {
+				if (type === 'text' && typeof(TelegramBot.triggers.text) !== 'undefined') {
+					const msg = TelegramBot.parseCommandString(item.message.text);
+					const obj = _.find(TelegramBot.triggers.text, obj => obj.command == msg[0]);
+					
+					if(obj) {
+						TelegramBot.send(obj.callback(msg, fromUsername, message), chatId);
+					} else if (TelegramBot.catchAllText.enabled) {
+						TelegramBot.catchAllText.callback(fromUsername, message);
+					}
+				} else if (typeof(TelegramBot.triggers[type]) !== 'undefined') {
 					TelegramBot.triggers[type].map(trigger => {
-						trigger.callback('N/A', from, message);
+						trigger.callback('N/A', fromUsername, message);
 					});
 				}
 			}
@@ -77,20 +87,22 @@ TelegramBot.parsePollResult = function(data) {
 	});
 }
 
-TelegramBot.requestUrl = function(method) {
+TelegramBot.requestUrl = method => {
 	const token = TelegramBot.token || process.env.TELEGRAM_TOKEN;
 	return TelegramBot.apiBase + token + '/' + method;
 }
 
-TelegramBot.addListener = function(command, callback, type = 'text') {
-	if(typeof(command) === 'string' && typeof(callback) === 'function') {
-		if(typeof(TelegramBot.triggers[type]) === 'undefined') {
+TelegramBot.addListener = (command, callback, type = 'text') => {
+	if (typeof(command) === 'string' && typeof(callback) === 'function') {
+		if (typeof(TelegramBot.triggers[type]) === 'undefined') {
 			TelegramBot.triggers[type] = [];
 		}
+
 		TelegramBot.triggers[type].push({
-			command: command,
-			callback: callback
+			command,
+			callback
 		});
+
 		console.log('Added command: ' + command);
 	}
 	else {
@@ -98,28 +110,38 @@ TelegramBot.addListener = function(command, callback, type = 'text') {
 	}
 }
 
-TelegramBot.startConversation = function(username, chat_id, callback, init_vars) {
-	if(typeof(username) === 'string' && typeof(callback) === 'function') {
-		if(typeof(TelegramBot.conversations[chat_id]) === 'undefined') {
+TelegramBot.startConversation = (username, chat_id, callback, init_vars) => {
+	if (typeof(username) === 'string' && typeof(callback) === 'function') {
+		if (typeof(TelegramBot.conversations[chat_id]) === 'undefined') {
 			TelegramBot.conversations[chat_id] = [];
 		}
-		if(typeof(init_vars) !== 'object') init_vars = {};
-		TelegramBot.conversations[chat_id].push(_.defaults(init_vars, { username: username, callback: callback}));
+
+		if (typeof(init_vars) !== 'object') {
+			init_vars = {};
+		}
+
+		TelegramBot.conversations[chat_id].push(_.defaults(init_vars, { username, callback }));
+		
 		console.log('Started conversation in Chat ID (' + chat_id + ') with ' + username);
-	} else
+	} else {
 		console.log('Error starting conversation in Chat ID (' + chat_id + ') with ' +  username);
+	}
 	//console.log('startConversation: Now we have ' + Object.keys(TelegramBot.conversations).length + ' chats with active conversations.');
 }
 
-TelegramBot.endConversation = function(username, chat_id) {
-	if(typeof(TelegramBot.conversations[chat_id]) !== 'undefined') {
+TelegramBot.endConversation = (username, chat_id) => {
+	if (typeof(TelegramBot.conversations[chat_id]) !== 'undefined') {
 		const obj = _.find(TelegramBot.conversations[chat_id], obj => obj.username == username);
-		if(obj) {
+		
+		if (obj) {
 			TelegramBot.conversations[chat_id] = _.reject(TelegramBot.conversations[chat_id], obj => obj.username == username);
-			if(_.isEmpty(TelegramBot.conversations[chat_id]))
+			
+			if (_.isEmpty(TelegramBot.conversations[chat_id])) {
 				TelegramBot.conversations = _.omit(TelegramBot.conversations, chat_id);
+			}
 			
 			console.log('Conversation ended with ' +  username + ' in Chat ID(' + chat_id + '). ' + Object.keys(TelegramBot.conversations).length + ' chats with active conversations remaining.');
+			
 			return true;
 		}
 	}
@@ -127,43 +149,46 @@ TelegramBot.endConversation = function(username, chat_id) {
 	//console.log('endConversation: Now we have ' + Object.keys(TelegramBot.conversations).length + ' chats with active conversations.');
 }
 
-TelegramBot.setCatchAllText = function(enabled, callback) {
+TelegramBot.setCatchAllText = (enabled, callback) => {
 	TelegramBot.catchAllText.enabled = enabled;
-	if(enabled) {
+
+	if (enabled) {
 		console.log('Registered catch-all for texts');
 		TelegramBot.catchAllText.callback = callback;
-	} else console.log('De-registered catch-all for texts');
+	} else {
+		console.log('De-registered catch-all for texts');
+	}
 };
 
-TelegramBot.method = function(method, object = {}) {
+TelegramBot.method = (method, params = {}) => {
 	try {
-		const res = HTTP.get(TelegramBot.requestUrl(method), {
-			params: object
-		});
+		const url = TelegramBot.requestUrl(method);
+		const res = HTTP.get(url, { params });
+		
 		if(res.data) {
 			return res.data;
 		}
-	}
-	catch (e) {
+	} catch (e) {
 		console.log('Error in polling: ' + e);
 		return false;
 	}
 }
 
-TelegramBot.send = function(msg, chatId, markdown) {
-	if(!msg) {
+TelegramBot.send = (msg, chatId, markdown) => {
+	if (!msg) {
 		return false;
 	}
 
-	if(markdown)
+	if (markdown) {
 		TelegramBot.method('sendMessage', {
 			chat_id: chatId,
 			text: msg,
 			parse_mode: 'Markdown'
 		});
-	else
+	} else {
 		TelegramBot.method('sendMessage', {
 			chat_id: chatId,
 			text: msg
 		});
+	}
 }
